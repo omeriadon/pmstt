e64ab331b6213635c798adc6cd8cf034dbaa58e7
Adon Omeri <adon.omeri@student.education.wa.edu.au>
2026-06-28 09:02:25 +0800

    Add production PM2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	apps: [
+		{
+			name: "timetable-api",
+			script: ".build/release/App",
+			cwd: "/var/www/timetable",
+			args: "serve --env production",
+			interpreter: "none",
+			env_production: {
+				HOSTNAME: "127.0.0.1",
+				PORT: "8080"
+			}
+		}
+	]
+}
