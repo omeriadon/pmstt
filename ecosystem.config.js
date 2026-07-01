4d1ea25011029583e9f6d1ab88ad5d4bb28d832e
Adon Omeri <adon.omeri@student.education.wa.edu.au>
2026-07-01 10:37:05 +0800

    add environment variables lol

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,6 +9,12 @@ module.exports = {
 			env_production: {
 				HOSTNAME: "127.0.0.1",
 				PORT: "8081"
+
+        APNS_TEAM_ID: "P6PV2R9443",
+				APNS_KEY_ID: "LS45S5RDJ2",
+        APNS_PRIVATE_KEY_PATH: "/etc/timetable/apns/AuthKey_KEYID.p8",
+				APNS_BUNDLE_ID: "com.omeriadon.Timetable",
+				APNS_USE_SANDBOX: "false"
 			}
 		}
 	]
