d8015e8863c9e3a6e7b7a9c38174535bf1d10065
Adon Omeri <adon.omeri@student.education.wa.edu.au>
2026-07-26 00:06:48 +0800

    minor

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,6 +10,8 @@ module.exports = {
 				HOSTNAME: "127.0.0.1",
 				PORT: "8081",
 
+				TIMETABLE_EVENT_ADMIN_EMAILS: "omeriadon@outlook.com"
+
 				APNS_TEAM_ID: "P6PV2R9443",
 				APNS_KEY_ID: "LS45S5RDJ2",
 				APNS_PRIVATE_KEY_PATH: "/etc/timetable/apns/AuthKey.p8",
