915de44f6b1c19a9a121a5b9c15e8045a0769522
Adon Omeri <adon.omeri@student.education.wa.edu.au>
2026-07-29 21:40:19 +0800

    fix authority migration retry

--- Sources/pmstt/Migrations/AddUserAccountAuthority.swift
+++ Sources/pmstt/Migrations/AddUserAccountAuthority.swift
@@ -1,10 +1,19 @@
 import Fluent
+import SQLKit
 
 struct AddUserAccountAuthority: AsyncMigration {
 	func prepare(on database: any Database) async throws {
-		try await database.schema(User.schema)
-			.field("account_authority", .string)
-			.update()
+		guard let sqlDatabase = database as? any SQLDatabase else {
+			throw AccountAuthorityMigrationError.unsupportedDatabase
+		}
+
+		if sqlDatabase.dialect.name == "postgresql" {
+			try await sqlDatabase.raw("ALTER TABLE \"users\" ADD COLUMN IF NOT EXISTS \"account_authority\" TEXT").run()
+		} else {
+			try await database.schema(User.schema)
+				.field("account_authority", .string)
+				.update()
+		}
 
 		try await User.query(on: database)
 			.set(\.$accountAuthority, to: .user)
@@ -17,9 +26,9 @@ struct AddUserAccountAuthority: AsyncMigration {
 				.update()
 		}
 
-		try await database.schema(User.schema)
-			.field("account_authority", .string, .required)
-			.update()
+		if sqlDatabase.dialect.name == "postgresql" {
+			try await sqlDatabase.raw("ALTER TABLE \"users\" ALTER COLUMN \"account_authority\" SET NOT NULL").run()
+		}
 	}
 
 	func revert(on database: any Database) async throws {
@@ -27,4 +36,8 @@ struct AddUserAccountAuthority: AsyncMigration {
 			.deleteField("account_authority")
 			.update()
 	}
+}
+
+private enum AccountAuthorityMigrationError: Error {
+	case unsupportedDatabase
 }
