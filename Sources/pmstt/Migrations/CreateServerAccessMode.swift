dd5aa6d8afe9a5bd08f9f8354bd54b5167d5013e
Adon Omeri <adon.omeri@student.education.wa.edu.au>
2026-07-29 21:51:19 +0800

    fix access mode migration retry

--- Sources/pmstt/Migrations/CreateServerAccessMode.swift
+++ Sources/pmstt/Migrations/CreateServerAccessMode.swift
@@ -1,17 +1,34 @@
 import Fluent
+import SQLKit
 
 struct CreateServerAccessMode: AsyncMigration {
 	func prepare(on database: any Database) async throws {
-		try await database.schema(ServerAccessMode.schema)
-			.id()
-			.field("development_access_only", .bool, .required)
-			.field("updated_at", .datetime)
-			.create()
+		guard let sqlDatabase = database as? any SQLDatabase else {
+			throw ServerAccessModeMigrationError.unsupportedDatabase
+		}
 
-		try await ServerAccessMode().create(on: database)
+		if sqlDatabase.dialect.name == "postgresql" {
+			try await sqlDatabase.raw("CREATE TABLE IF NOT EXISTS \"server_access_mode\" (\"id\" UUID PRIMARY KEY, \"development_access_only\" BOOL NOT NULL, \"updated_at\" TIMESTAMPTZ)").run()
+		} else {
+			try await database.schema(ServerAccessMode.schema)
+				.id()
+				.field("development_access_only", .bool, .required)
+				.field("updated_at", .datetime)
+				.create()
+		}
+
+		guard try await ServerAccessMode.query(on: database).first() == nil else {
+			return
+		}
+
+		try await ServerAccessMode(developmentAccessOnly: false).create(on: database)
 	}
 
 	func revert(on database: any Database) async throws {
 		try await database.schema(ServerAccessMode.schema).delete()
 	}
+}
+
+private enum ServerAccessModeMigrationError: Error {
+	case unsupportedDatabase
 }
