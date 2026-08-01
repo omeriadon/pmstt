07e4a1fa5995d0d83567947d0fa9df6a1a3e59be
Adon Omeri <adon.omeri@student.education.wa.edu.au>
2026-08-01 14:39:00 +0800

    fix broadcast deletion migration

--- Sources/pmstt/Migrations/AddBroadcastNotificationDeletion.swift
+++ Sources/pmstt/Migrations/AddBroadcastNotificationDeletion.swift
@@ -3,7 +3,7 @@ import Fluent
 struct AddBroadcastNotificationDeletion: AsyncMigration {
 	func prepare(on database: any Database) async throws {
 		try await database.schema(BroadcastNotificationRecord.schema)
-			.field("is_deleted", .bool, .required, .default(false))
+			.field("is_deleted", .bool, .required, .sql(.default(false)))
 			.update()
 	}
 
