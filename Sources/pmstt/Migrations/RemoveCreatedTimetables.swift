8361e8413d7386b2ceda8cd576c99c3e2860ea4b
Adon Omeri <adon.omeri@student.education.wa.edu.au>
2026-08-08 07:25:20 +0800

    remove legacy created timetable migrations

--- Sources/pmstt/Migrations/RemoveCreatedTimetables.swift
+++ Sources/pmstt/Migrations/RemoveCreatedTimetables.swift
@@ -3,7 +3,26 @@ import SQLKit
 
 struct RemoveCreatedTimetables: AsyncMigration {
 	func prepare(on database: any Database) async throws {
-		if let sqlDatabase = database as? any SQLDatabase {
+		guard let sqlDatabase = database as? any SQLDatabase else {
+			return
+		}
+
+		let createdTimetableCount: Int
+		if sqlDatabase.dialect.name == "postgresql" {
+			createdTimetableCount = try await sqlDatabase.raw("""
+				SELECT COUNT(*) AS "count"
+				FROM information_schema.tables
+				WHERE table_name = 'created_timetables'
+				""").first(decodingColumn: "count", as: Int.self) ?? 0
+		} else {
+			createdTimetableCount = try await sqlDatabase.raw("""
+				SELECT COUNT(*) AS "count"
+				FROM sqlite_master
+				WHERE type = 'table' AND name = 'created_timetables'
+				""").first(decodingColumn: "count", as: Int.self) ?? 0
+		}
+
+		if createdTimetableCount > 0 {
 			try await sqlDatabase.raw("""
 				DELETE FROM "received_timetable_imports"
 				WHERE "source_kind" = 'createdForThirdParty'
@@ -16,12 +35,13 @@ struct RemoveCreatedTimetables: AsyncMigration {
 				DELETE FROM "pass_records"
 				WHERE "source_kind" = 'createdForThirdParty'
 				""").run()
+
+			try await database.schema("pass_records")
+				.deleteField("created_timetable_id")
+				.update()
 		}
 
-		try await database.schema("pass_records")
-			.deleteField("created_timetable_id")
-			.update()
-		try await database.schema("created_timetables").delete()
+		try await sqlDatabase.raw("DROP TABLE IF EXISTS \"created_timetables\"").run()
 	}
 
 	func revert(on _: any Database) async throws {}
